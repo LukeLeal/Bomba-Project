6b4719f86b5327b0a2df2458189685dba658235f
Lucas Leal Mendonça <lukelealm@gmail.com>
2017-10-20 12:50:16 -0200

    Beta Scripts criados. Boneco colocado na cena
    Esses controller scripts provavelmente não serão finais, mas me darão uma ideia de "how to grid".

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : Singleton<GridController> {
+
+    public Grid grid;
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Grid info \nCell Size: " + grid.cellSize
+            + "\nCell gap: " + grid.cellGap
+            //+ "\nCell "+grid.
+            );
+        GameObject boneco = GameObject.FindWithTag("Player");
+        boneco.transform.position = grid.GetCellCenterWorld(grid.WorldToCell(boneco.transform.position));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<Instance> : MonoBehaviour where Instance : Singleton<Instance> {
+    public static Instance instance;
+    public bool isPersistant;
+
+    /// <summary>
+    /// Awake this instance.
+    /// </summary>
+    public virtual void Awake() {
+        if (isPersistant) {
+            // Singleton persistente entre as sessões de jogo
+
+            if (!instance) {
+                instance = this as Instance;
+            } else {
+                DestroyObject(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+
+        } else {
+            // Singleton simples
+            instance = this as Instance;
+        }
+    }
+}
